0bf977cb8f6d6e41c60b91311a88e8d843a4fbf9
Kate <kate.stark13@sbcglobal.net>
2018-04-14 09:25:29 -0500

    updated functionality

--- bamazon.sql
+++ bamazon.sql
@@ -9,7 +9,21 @@ CREATE TABLE products(
 	item_id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(100) NOT NULL,
 	department_name VARCHAR(50) NOT NULL,
-	price INT default 0,
+	price DECIMAL (10,2) DEFAULT NULL,
 	stock_quantity INT default 0,
 	PRIMARY KEY (item_id)
 );
+
+/*add items to the table*/
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES 
+		(1, 'Christmas Tree Lights', 'Hoiday', 12.99, 1000),
+		(2, 'Bath Towels (Red)', 'Home', 13.99, 200),
+		(3, 'Bananas', 'Grocery', 0.60, 2000),
+		(4, 'Womens Leggings', 'Clothing', 20.00, 50),
+		(5, 'Backpack', 'Travel', 40.00, 19),
+		(6, 'Night Light', 'Home', 8.00, 40),
+		(7, 'Fitbit', 'Exercise', 200.00, 30)
+		(8, 'Sidewalk Chalk', 'Toys', 5.99, 100),
+		(9, 'Aluminum Baseball Bat', 'Sporting Goods', 30.00, 77),
+		(10, 'Mascara', 'Makeup', 18.00, 88);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require("inquirer");
 //create the connection for sql database
 var connection = mysql.createConnection({
 	host:"localhost",
-	port:3000,
+	port:3306,
 
 	//username
 	user: "root",
@@ -13,16 +13,70 @@ var connection = mysql.createConnection({
 	database: "bamazon_DB"
 });
 
+
 connection.connect(function(err){
 	if(err) throw err;
-	displayInventory();
+	start();
 });
 
 //will display all items in inventory for the user to select
-function displayInventor(){
+function start(){
+	
+	//make db query
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
 
+		console.log("Existing Inventory: ");
+		console.log("---------------------");
+
+		for ( var i = 0; i < res.length; i++) {
+
+			console.log("Item ID: " + res[i].item_id  + " | " + "Product Name: " + res[i].product_name + " | " + "Department Name: " + res[i].department_name + " | " + "Price: $" + res[i].price);
+		}
+		console.log("--------------------");
+
+	});
+	console.log(" ");
+	inquirer.prompt([
+		//prompt for ID of the item to purchase
+		{
+			type: "input",
+			name: "id",
+			message: "What is the Id of the product you would like to purchase?",
+			validate: function(value){
+				if(isNaN(value) == false && parseInt(value) <= res.length && parse.Int(value) > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		//prompt for the quantity to purchase
+		{
+			type:"input",
+			name:"qty",
+			message: "How many would you like to purchase?",
+			validate: function(value) {
+				if(isNaN(value)) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		};
+		]).then(function(ans){
+			var whatToBuy = (ans.id)-1;
+			var howMuchToBuy = parseInt(ans.qty);
+			var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
+
+			//check if item is in stock
+			if(res[whatToBuy].stock_quantity >= howMuchToBuy){
+				//update quantity of product number after purchase
+				connection.query("UPDATE ")
+			}
+		})
 };
-
+/*
 //make sure users input is valid
 function validateInput(){
 
@@ -31,4 +85,5 @@ function validateInput(){
 //will prompt the user what item/quantity they would like to purchase
 function promptUserPurchase(){
 
-};
+};*/
+
